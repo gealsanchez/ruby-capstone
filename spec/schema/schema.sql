a32585b376f3cadc5b97e36e53f0bb11b13866b7
gealsanchez <gealsanchez@gmail.com>
2023-05-04 07:01:43 -0500

    Schema added

--- spec/schema/schema.sql
+++ spec/schema/schema.sql
@@ -1,6 +1,18 @@
 -- Create Database
 CREATE DATABASE ruby_catalog;
 
+-- Item Table
+CREATE TABLE item (
+  id              integer GENERATED ALWAYS AS IDENTITY,
+  genre           varchar(100),
+  author          varchar(100),
+  source          varchar(100),
+  label           varchar(100),
+  publish_date    date,
+  archived        boolean
+  PRIMARY KEY(id)
+);
+
 -- Genres Table
 CREATE TABLE genre(
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -13,4 +25,19 @@ CREATE TABLE music_album(
   album_name VARCHAR(150) NOT NULL,
   on_spotify BOOLEAN NOT NULL,  
   publish_date DATE NOT NULL 
+);
+
+-- Books Table
+CREATE TABLE books (
+  id INT,
+	publisher  varchar(100),
+	cover_state varchar(100),
+	FOREIGN KEY(id) REFERENCES item(id)
+);
+
+-- Labels Table
+CREATE TABLE labels (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  title varchar(100),
+  color varchar(100)
 );

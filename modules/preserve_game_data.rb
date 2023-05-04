f5fcdbb284649207b96723e33ab041493c5dba4e
gealsanchez <gealsanchez@gmail.com>
2023-05-04 12:21:14 -0500

    Preserve date files updated

--- modules/preserve_game_data.rb
+++ modules/preserve_game_data.rb
@@ -1,14 +1,14 @@
 module GameData
   def save_game_data
     games_data = @games.map(&:to_hash)
-    File.write('data/books.json', JSON.pretty_generate(books_data))
+    File.write('data/games.json', JSON.pretty_generate(games_data))
   end
 
   def load_game_data
     return [] unless File.exist?('data/games.json')
 
     JSON.parse(File.read('data/games.json')).map do |match|
-      auhor = Label.new(match['label']['title'], match['label']['color'])
+      author = Author.new(match['author']['first_name'], match['author']['last_name'])
       game = Game.new(match['publish_date'], match['last_played_date'], match['multiplayer'])
       game.author = author
       game

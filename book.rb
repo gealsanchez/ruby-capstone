cb61a7890cd2afd9c654e8f7168b2b68373e88c9
fithamlak <justiceofgod01@gmail.com>
2023-05-04 08:13:25 +0300

    add to_hash function to book class to preserve data

--- book.rb
+++ book.rb
@@ -3,7 +3,7 @@ require_relative './item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, publish_date, _archived: false)
+  def initialize(publisher, cover_state, publish_date)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
@@ -11,5 +11,14 @@ class Book < Item
 
   def can_be_archived?
     cover_state == 'bad' ? true : super
+  end
+
+  def to_hash
+    {
+      publisher: @publisher,
+      publish_date: @publish_date,
+      cover_state: @cover_state,
+      label: { title: label.title, color: label.color }
+    }
   end
 end
